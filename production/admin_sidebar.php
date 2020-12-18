2ddee4a49478a2b19fca2da92e4dbd8c64298c99
christian <christian.odigbo73@gmail.com>
2020-12-18 16:38:10 +0100

    Fifth Commit

---
+++ production/admin_sidebar.php
@@ -0,0 +1,20 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+	<div class="menu_section">
+		<h3>General</h3>
+		<ul class="nav side-menu">
+			<li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="admin1.php">Masters List</a></li>
+					<li><a href="admin_list_talisay.php">AIFPU | OCS</a></li>
+					<li><a href="admin_list_alijis.php">AIFPU | OCS</a></li>
+					<li><a href="admin_list_binalbagan.php">AIFPU | OCS</a></li>
+					<li><a href="admin_list_ftowne.php">AIFPU | OCS</a></li>
+					<li><a href="admin_list_allcleared.php">List of All Cleared Student</a></li>
+					<li><a href="admin_list_allinactive.php">List of All Inactive Student</a></li>
+					<li><a href="admin.php">List by Department</a></li>
+					<li><a href="javascript:;" data-toggle="modal" data-target="#set_deadline">Set deadline</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
